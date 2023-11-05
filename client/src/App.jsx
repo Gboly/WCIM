cd67a0cc070f103ad291dbe5a254f5401184350f
Gboly <gbolahanahmed1@gmail.com>
2023-11-05 19:49:06 +0100

    feat: Set default theme based on saved setting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
+import { useLayoutEffect } from "react";
 import "./App.css";
+import { getTheme } from "./util/functions";
 
 function App({ children }) {
   // Treat top-level states and contexts here
+
+  useLayoutEffect(() => {
+    const theme = getTheme();
+    document.querySelector("html").setAttribute("data-theme", theme);
+  }, []);
 
   return <>{children}</>;
 }
---
+++ client/src/util/functions.js
@@ -0,0 +1,14 @@
+export const getTheme = () => {
+  const localStorageTheme = localStorage.getItem("theme");
+  const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+  if (localStorageTheme !== null) {
+    return localStorageTheme;
+  }
+
+  if (systemSettingDark.matches) {
+    return "dark";
+  }
+
+  return "light";
+};
