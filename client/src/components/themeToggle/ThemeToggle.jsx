7856b6349d4de9c8f0e8f9912631b9536397f525
Gboly <gbolahanahmed1@gmail.com>
2023-11-05 20:45:36 +0100

    feat: Implement theme toggle

--- client/src/components/themeToggle/ThemeToggle.jsx
+++ client/src/components/themeToggle/ThemeToggle.jsx
@@ -2,12 +2,38 @@ import DarkModeIcon from "@mui/icons-material/DarkMode";
 import LightModeIcon from "@mui/icons-material/LightMode";
 import { iconStyle } from "../../util/style";
 import Icon from "../icon/Icon";
+import { useEffect, useLayoutEffect, useState } from "react";
 
 function ThemeToggle() {
+  const [theme, setTheme] = useState(null);
+
+  useLayoutEffect(() => {
+    const currentTheme = document
+      .querySelector("html")
+      .getAttribute("data-theme");
+    setTheme(currentTheme);
+  }, []);
+
+  useEffect(() => {
+    const currentTheme = document
+      .querySelector("html")
+      .getAttribute("data-theme");
+
+    if (theme && currentTheme !== theme) {
+      document.querySelector("html").setAttribute("data-theme", theme);
+      localStorage.setItem("theme", theme);
+    }
+  }, [theme]);
+
+  const handleClick = () => setTheme(theme === "light" ? "dark" : "light");
+
   return (
-    <Icon>
-      {/* <DarkModeIcon style={iconStyle} /> */}
-      <LightModeIcon style={iconStyle} />
+    <Icon {...{ handleClick }}>
+      {theme === "dark" ? (
+        <DarkModeIcon style={iconStyle} />
+      ) : (
+        <LightModeIcon style={iconStyle} />
+      )}
     </Icon>
   );
 }
