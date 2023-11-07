b95ffc15b8f08a1f663b1424d15b1285d10d323a
Gboly <gbolahanahmed1@gmail.com>
2023-11-07 11:28:15 +0100

    feat: Create custom hook to handle pop-ups

---
+++ client/src/util/cutom-hooks/usePopupHandler.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+const usePopUpHandler = (popUpRef) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isListeningActive, setIsListeningActive] = useState(false);
+
+  useEffect(() => {
+    if (isOpen) {
+      if (isListeningActive) {
+        const initiateClose = (e) =>
+          !popUpRef.current.contains(e.target) && hide();
+        document.addEventListener("click", initiateClose);
+        return () => document.removeEventListener("click", initiateClose);
+      } else {
+        setIsListeningActive(true);
+      }
+    }
+  }, [isOpen, isListeningActive, popUpRef]);
+
+  const show = () => setIsOpen(true);
+  const hide = () => {
+    setIsListeningActive(false);
+    setIsOpen(false);
+  };
+
+  return [isOpen, show];
+};
+
+export default usePopUpHandler;
