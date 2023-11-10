11b044c56b574fd6e77a4ea28bed91a03f4df4ce
Gboly <gbolahanahmed1@gmail.com>
2023-11-10 23:53:56 +0100

    feat: Set up content for what-we-do section

--- client/src/util/content.js
+++ client/src/util/content.js
@@ -20,31 +20,37 @@ export const navOptions = [
         icon: evangelism,
         desc: "Evangelism",
         url: "/what-we-do/evangelism",
+        body: "Spiritual well-being is integral to holistic growth. Your support allows us to spread hope and faith, bringing comfort to those in despair. Join us in sharing the transformative power of love, compassion, and faith within communities.",
       },
       {
         icon: water,
         desc: "Clean water",
         url: "/what-we-do/clean-water",
+        body: "Access to clean water is a global crisis, affecting millions. Contaminated water causes diseases, posing severe health risks. Your contribution empowers us to provide clean water sources, ensuring healthier communities and brighter futures.",
       },
       {
         icon: book,
         desc: "Education",
         url: "/what-we-do/education",
+        body: "Limited access to education perpetuates poverty and inequality. By supporting our educational initiatives, you empower underprivileged children, breaking the cycle of poverty and opening doors to a brighter, more equitable future.",
       },
       {
         icon: health,
-        desc: "Health",
+        desc: "Health care",
         url: "/what-we-do/health",
+        body: "Inadequate health care perpetuates a cycle of preventable illnesses and suffering. Your support enables us to offer medical services to those in need, ensuring a healthier community. Together, we can combat illness, improve well-being, and save lives.",
       },
       {
         icon: orphanage,
         desc: "Orphanage",
         url: "/what-we-do/orphanage",
+        body: "Orphans face emotional and physical challenges without stable homes. Your contribution will help in supporting orphanages, providing love, care, and opportunities for a better future. Help us create a nurturing environment for these children.",
       },
       {
         icon: money,
         desc: "Financing",
         url: "/what-we-do/financing",
+        body: "Financial instability hinders personal and community growth. Your contribution empowers us to provide financial support, fostering economic independence. Together, we can uplift individuals and communities, breaking the chains of poverty.",
       },
     ],
     columnType: "triple-column",
@@ -97,3 +103,7 @@ export const navOptions = [
     columnType: "single-column",
   },
 ];
+
+export const doingsContent = navOptions.find(
+  (option) => option.desc === "What we do"
+).content;
