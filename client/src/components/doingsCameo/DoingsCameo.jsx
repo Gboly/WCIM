067fcee0d97bf46e1eca7ed77fc3fed1c4641098
Gboly <gbolahanahmed1@gmail.com>
2023-11-11 08:40:09 +0100

    fix: Make the doingsCameo a link

--- client/src/components/doingsCameo/DoingsCameo.jsx
+++ client/src/components/doingsCameo/DoingsCameo.jsx
@@ -3,6 +3,7 @@ import { forwardRef } from "react";
 import Animated from "../animated/Animated";
 import { motion } from "framer-motion";
 import { hSlideInLeft, hSlideInRight } from "../../util/variants";
+import { NavLink } from "react-router-dom";
 
 const isLeft = (index) => index % 2 === 0;
 const delayByIndex = [0.8, 0.8, 1.3, 1.3, 0, 0];
@@ -17,22 +18,24 @@ const DoingsCameoWithRef = (
   };
 
   return (
-    <Animated
-      element={motion.div}
-      className="doingsCameo"
-      variants={
-        isLeft(index) ? hSlideInLeft(transition) : hSlideInRight(transition)
-      }
-      ref={ref}
-      useVariantHover={true}
-      animateExit={index > 3}
-    >
-      <img src={icon} alt={"water"} />
-      <article>
-        <header>{desc}</header>
-        <p>{body}</p>
-      </article>
-    </Animated>
+    <NavLink to={url}>
+      <Animated
+        element={motion.div}
+        className="doingsCameo"
+        variants={
+          isLeft(index) ? hSlideInLeft(transition) : hSlideInRight(transition)
+        }
+        ref={ref}
+        useVariantHover={true}
+        animateExit={index > 3}
+      >
+        <img src={icon} alt={"water"} />
+        <article>
+          <header>{desc}</header>
+          <p>{body}</p>
+        </article>
+      </Animated>
+    </NavLink>
   );
 };
 
