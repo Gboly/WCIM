d862de82eccc08ca6b6b0d7ede0b77d81a017dd2
Gboly <gbolahanahmed1@gmail.com>
2023-11-15 21:38:23 +0100

    fix: Update the footer setion

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -4,6 +4,13 @@ import { forwardRef, useRef, useState } from "react";
 import Animated from "../animated/Animated";
 import { AnimatePresence, motion } from "framer-motion";
 import { spreadOut } from "../../util/variants";
+import facebook from "../../assets/facebook.png";
+import instagram from "../../assets/instagram.png";
+import gmail from "../../assets/gmail.png";
+import Brand from "../brand/Brand";
+import Icon from "../icon/Icon";
+import PlaceIcon from "@mui/icons-material/Place";
+import CallIcon from "@mui/icons-material/Call";
 
 const extendedMissionStatement = [
   "Operation Blessing is a nonprofit, humanitarian organization dedicated to partnering with YOU to demonstrate God’s love by alleviating human suffering in the U.S. and around the world.",
@@ -11,6 +18,15 @@ const extendedMissionStatement = [
   "Through compassionate, efficient, and locally-focused programs we are able to target the specific needs of those we serve and provide a gateway for our partners to truly be the hands and feet of Jesus for those who are suffering and in need.",
 ];
 const learnMorecontent = ["Give Now", "About Us", "Contact Us"];
+const socials = [
+  {
+    name: "facebook",
+    src: facebook,
+    url: "https://web.facebook.com/myworldcare208",
+  },
+  { name: "instagram", src: instagram, url: "" },
+  { name: "gmail", src: gmail, url: "mailto:worldcareinc@gmail.com" },
+];
 
 const Footer = () => {
   const [userDetails, setUserDetails] = useState({ name: "", email: "" });
@@ -61,8 +77,26 @@ const Footer = () => {
               />
               <button type="submit">KEEP ME INFORMED</button>
             </form>
+            <div>
+              {socials.map(({ name, src, url }) => (
+                <a key={name} href={url} target="_blank" rel="noreferrer">
+                  <img src={src} alt={`${name} icon`} />
+                </a>
+              ))}
+            </div>
           </FooterSection>
         </main>
+        <div>
+          <Brand />
+          <div>
+            <Icon value={PlaceIcon} size={1.2} />
+            <span>Springfield, IL, United States, Illinois</span>
+          </div>
+          <div>
+            <Icon value={CallIcon} size={1.2} />
+            <a href="tel:+1 518-986-1836">+1 518-986-1836</a>
+          </div>
+        </div>
       </footer>
       <footer className="footer-2">
         <p>
