087e4cd17feee7d81cb9bacd0ccfaa5f7c177040
Gboly <gbolahanahmed1@gmail.com>
2023-11-15 23:22:20 +0100

    fix: Dynamically render story details

--- client/src/components/storyCameo/StoryCameo.jsx
+++ client/src/components/storyCameo/StoryCameo.jsx
@@ -1,5 +1,4 @@
 import "./storyCameo.css";
-import placeholder from "../../assets/wcim-main-pic.jpg";
 import Article from "../article/Article";
 import { NavLink } from "react-router-dom";
 import { useRef, useState } from "react";
@@ -14,7 +13,7 @@ import {
 } from "../../util/variants";
 
 const frameColors = ["#43409f", "#ed1414", "#009a00"];
-const StoryCameo = ({ index }) => {
+const StoryCameo = ({ index, content: { imgSrc, desc, body, url } }) => {
   const storyCameoRef = useRef(null);
   const [isReferenceHover, setIsReferenceHover] = useState(false);
 
@@ -40,10 +39,10 @@ const StoryCameo = ({ index }) => {
           isReferenceHover={isReferenceHover}
           style={{ backgroundColor: frameColors[index] }}
         />
-        <NavLink>
+        <NavLink to={url}>
           <Animated
             element={motion.img}
-            src={placeholder}
+            src={imgSrc}
             alt={"stoy poster image"}
             ref={storyCameoRef}
             variants={justReactions(imageScale)}
@@ -56,13 +55,7 @@ const StoryCameo = ({ index }) => {
         variants={isReverse ? hSlideInLeft() : hSlideInRight()}
         ref={storyCameoRef}
       >
-        <Article
-          header={"Borehole water in Modakeke, Osun state, Nigeria."}
-          body={
-            "ISRAEL – As the war in Israel continues, many families fleeing the conflict are left wondering where their next meal might come from. Because of your faithful support, Operation Blessing is on the ground doing everything we can to provide hot meals for hurting families in Israel. In partnership with CBN Israel, we’re operating a"
-          }
-          url={`/stories/clean-water/12345`}
-        />
+        <Article header={desc} body={body.slice(0, 300)} url={url} />
       </Animated>
     </div>
   );
