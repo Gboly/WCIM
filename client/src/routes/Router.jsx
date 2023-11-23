a66c03ea7073b3a65f2511174495f3d13126e75a
Gboly <gbolahanahmed1@gmail.com>
2023-11-23 14:01:54 +0100

    feat: Setup routing for doings pages

--- client/src/routes/Router.jsx
+++ client/src/routes/Router.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import App from "../App";
 import Layout from "../layout/Layout";
 import Home from "../pages/home/Home";
+import WhatWeDo from "../pages/what-we-do/WhatWeDo";
+import { doingsPageContent } from "../util/content";
 
 export default function Router() {
   return (
@@ -15,6 +17,13 @@ export default function Router() {
         }
       >
         <Route index element={<Home />} />
+        {doingsPageContent.map((content) => (
+          <Route
+            key={content.id}
+            path={`what-we-do/${content.id}`}
+            element={<WhatWeDo content={content} />}
+          />
+        ))}
       </Route>
     </Routes>
   );
