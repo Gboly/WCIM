27a294d92437be9deb021601473a5b5accca443e
Gboly <gbolahanahmed1@gmail.com>
2023-11-26 20:00:36 +0100

    feat: Update Content for what-we-do pages

--- client/src/util/content.js
+++ client/src/util/content.js
@@ -16,6 +16,7 @@ import facebook from "../assets/facebook.png";
 import instagram from "../assets/instagram.png";
 import gmail from "../assets/gmail.png";
 import cleanWaterVideo from "../assets/clean-water-vid.mp4";
+import educationVideo from "../assets/education-vid.mp4";
 
 export const navOptions = [
   {
@@ -178,5 +179,75 @@ export const doingsPageContent = [
       "The need is urgent, and YOUR support is the catalyst for change. Join us in our quest to create a world where clean water flows freely, a testament to the power of collective action in addressing one of humanity's most critical challenges. YOUR donation isn't just a drop; it's a ripple of hope shaping a better future for communities worldwide. Together, let's make waves of lasting change.",
     ],
     video: cleanWaterVideo,
+  },
+  {
+    id: "evangelism",
+    title: "Evangelism",
+    poster: boreholeAponmu,
+    icon: evangelism,
+    snippet:
+      "Evangelism addresses the human search for spiritual fulfillment by sharing a transformative message. It builds communities, positively influences society, and provides hope and guidance in challenging times. Beyond individual transformation, evangelism creates a global sense of unity, connecting believers in a shared spiritual journey.",
+    content: [
+      "YOUR contribution to evangelism serves as a divine beacon, spreading hope through the teachings of Jesus. By generously supporting, YOU enable the creation of educational materials and outreach programs that embody Christian values.",
+      "YOUR donations, guided by the spirit of charity, become tangible expressions of Christ's love, reaching diverse communities in their spiritual and material needs. In aligning YOUR support with evangelism, YOU become an agent of transformative change, reflecting the compassion and teachings of Jesus in the world.",
+      "Moreover, YOUR support often amplifies philanthropic initiatives embedded within evangelism, such as healthcare missions, educational programs, and community development projects, manifesting the teachings of Jesus in tangible ways. Through these divine efforts, YOU become a vessel of transformation, uplifting lives on both spiritual and practical levels in alignment with the compassionate spirit of Christianity.",
+    ],
+    video: "",
+  },
+  {
+    id: "education",
+    title: "Education",
+    poster: boreholeAponmu,
+    icon: book,
+    snippet:
+      "Across the globe, hundreds of thousands of individuals face educational challenges that could be alleviated with proper support. Unfortunately, many of them lack the financial means to access the educational resources and opportunities they desperately need.",
+    content: [
+      "YOUR generous support is vital in overcoming educational challenges in developing countries, where financial constraints hinder access to quality learning. Scholarships and free education initiatives, fueled by YOUR generosity, aim to bridge this gap and empower aspiring minds.",
+      "In Nigeria, YOUR impactful support funds scholarships and free education programs, creating opportunities for numerous students to pursue their educational dreams. These initiatives break the cycle of limited opportunities, providing a lifeline for individuals to escape poverty through knowledge and empowerment.",
+      "The success in Nigeria fuels our global commitment. With YOUR support, we aim to replicate transformative education globally, empowering individuals, uplifting communities, and fostering a brighter, educated future.",
+    ],
+    video: educationVideo,
+  },
+  {
+    id: "health",
+    title: "Health care",
+    poster: boreholeAponmu,
+    icon: health,
+    snippet:
+      "Globally, hundreds of thousands are afflicted by medical conditions that could be alleviated with proper care. Unfortunately, many lack the financial means to access the medical care they require.",
+    content: [
+      "YOUR support is crucial in providing medical care to those in need. Although, efforts have been made in Nigeria, we need to do more and also in other parts of the world as global shortage of accessible healthcare persists, leading to profound consequences and preventable deaths.",
+      "Worldwide, untreated medical conditions highlight the urgent need for expanded healthcare initiatives. The lack of timely interventions intensifies health challenges, making your support vital in realizing medical care as a fundamental human right for those in crisis.",
+      "As we strive to expand these efforts globally, your support becomes instrumental in saving lives, alleviating suffering, and advocating for healthcare accessibility worldwide. The vision is to create a world where no one is left behind due to a lack of medical care, and your support is a powerful force propelling us toward that transformative goal.",
+    ],
+    video: "",
+  },
+  {
+    id: "orphanage",
+    title: "Orphanage",
+    poster: boreholeAponmu,
+    icon: orphanage,
+    snippet:
+      "Across the world, hundreds of thousands of orphans endure challenging circumstances such as inadequate care, limited educational opportunities, and emotional hardship, which could be improved with proper support. Regrettably, many lack the necessary resources to access the care and opportunities they deserve.",
+    content: [
+      "YOUR support is a lifeline for orphans, providing essential assistance in orphanages. In Nigeria, progress has been made, but a global initiative is crucial. Without robust support, orphans face inadequate care, limited educational opportunities, and emotional challenges. YOUR support is vital in addressing these issues and extending hope for a brighter future.",
+      "With YOUR backing, we aim to broaden our impact globally, ensuring orphans worldwide receive the care and opportunities they deserve.",
+      "The lack of such support adversely affects orphans, hindering their physical, educational, and emotional well-being. YOUR contribution transcends borders, breaking down financial barriers and offering orphans a chance at a more promising future. Together, we can make a transformative impact, providing these children with the support they need to overcome challenges and fostering a more compassionate global future.",
+    ],
+    video: "",
+  },
+  {
+    id: "financing",
+    title: "Finanacial support",
+    poster: boreholeAponmu,
+    icon: money,
+    snippet:
+      "Particularly in developing nations, individuals possess business ideas, inventions, and skills that could transform their lives and communities. However, the lack of financial resources hinders them from turning these aspirations into reality. Your support becomes a catalyst for a change.",
+    content: [
+      "In many developing regions worldwide, individuals possess untapped potential in business, innovation, and valuable skills. However, financial barriers often hinder the realization of these aspirations. YOUR vital support plays a key role in removing these obstacles by providing essential funds for business ventures, innovative projects, and skill enhancement, empowering individuals to turn their potential into tangible progress and economic independence.",
+      "By investing in these individuals, YOUR support becomes a transformative force, not only changing individual destinies but also catalyzing positive shifts within entire communities. The establishment of businesses and realization of inventions create sustainable sources of income, breaking the chains of poverty.",
+      "Through YOUR generosity, YOU sow the seeds of self-sufficiency, fostering a ripple effect that uplifts individuals, families, and entire communities towards a brighter and more sustainable future.",
+    ],
+    video: "",
   },
 ];
