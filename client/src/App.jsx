569846a65e17aded7810d68fd4a5f8803322c08d
Gboly <gbolahanahmed1@gmail.com>
2023-11-27 17:04:33 +0100

    fix: Render every page from top onNavigating

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,22 @@
 import { useLayoutEffect } from "react";
 import "./App.css";
 import { getTheme } from "./util/functions";
+import { useLocation } from "react-router-dom";
 
 function App({ children }) {
   // Treat top-level states and contexts here
+
+  const location = useLocation();
 
   useLayoutEffect(() => {
     const theme = getTheme();
     document.querySelector("html").setAttribute("data-theme", theme);
   }, []);
+
+  // Each page should be rendered from top.
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
 
   return <>{children}</>;
 }
