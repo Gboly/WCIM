97f302ff9fcf54287ca72beed226b168ef9d6830
Gboly <gbolahanahmed1@gmail.com>
2023-12-04 00:34:31 +0100

    feat: Create a separate component for banner

---
+++ client/src/components/banner/Banner.jsx
@@ -0,0 +1,31 @@
+import { forwardRef } from "react";
+import "./banner.css";
+import Animated from "../animated/Animated";
+import { motion } from "framer-motion";
+import { hSlideIn } from "../../util/variants";
+import givingPoster from "../../assets/wcim-giving-poster.png";
+
+const BannerWithRef = ({ desc }, ref) => {
+  return (
+    <section
+      ref={ref}
+      className="reference-container banner"
+      id="giving-catalog"
+    >
+      <Animated
+        element={motion.div}
+        variants={hSlideIn({})}
+        ref={ref}
+        useVariantHover={true}
+      >
+        <img src={givingPoster} alt="giving icon" />
+        <div>
+          <p>{desc}</p>
+        </div>
+      </Animated>
+    </section>
+  );
+};
+
+const Banner = forwardRef(BannerWithRef);
+export default Banner;
