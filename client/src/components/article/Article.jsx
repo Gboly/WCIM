38e62e7e47652f7c071fc1e0f0060060b4f71784
Gboly <gbolahanahmed1@gmail.com>
2023-12-04 20:57:41 +0100

    fix: Modify Article to render quotes

--- client/src/components/article/Article.jsx
+++ client/src/components/article/Article.jsx
@@ -1,18 +1,29 @@
 import { NavLink } from "react-router-dom";
 import "./article.css";
 
-const Article = ({ header, body, url, handleClick }) => {
+const Article = ({ header, body, url, handleClick, quote }) => {
   return (
     <article className="article">
       <header>{header}</header>
-      <p>
-        {body}
-        {(url || handleClick) && (
-          <span onClick={handleClick}>
-            <NavLink to={url}>Read more...</NavLink>
-          </span>
-        )}
-      </p>
+      {quote ? (
+        <q>
+          {body}
+          {(url || handleClick) && (
+            <span onClick={handleClick}>
+              <NavLink to={url}>Read more...</NavLink>
+            </span>
+          )}
+        </q>
+      ) : (
+        <p>
+          {body}
+          {(url || handleClick) && (
+            <span onClick={handleClick}>
+              <NavLink to={url}>Read more...</NavLink>
+            </span>
+          )}
+        </p>
+      )}
     </article>
   );
 };
