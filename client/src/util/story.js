56543343664dc873d945bb8ca974a69a48488b99
Gboly <gbolahanahmed1@gmail.com>
2023-12-05 09:20:05 +0100

    fix: Update story content

--- client/src/util/story.js
+++ client/src/util/story.js
@@ -1,5 +1,14 @@
+import osoloOfOsolo from "../assets/wcim-osolo.jpg";
+import cleanWaterVideo from "../assets/clean-water-vid.mp4";
+import educationVideo from "../assets/clean-water-vid.mp4";
+
 export const aponmuBoreholeStory = [
   "In the heart of Ondo State, Nigeria, lies the resilient community of Aponmu. For years, the lack of access to clean water had been an enduring challenge, casting a shadow over the daily lives of its residents. Women and children traversed long distances, bearing the burden of water containers, their daily routines dictated by the relentless pursuit of this basic necessity.",
+  {
+    type: "video",
+    src: cleanWaterVideo,
+    desc: "A documentry on the issues faced due to lack of water.",
+  },
   "The community's plea for relief echoed far and wide, reaching the ears of our charity organization. Determined to make a lasting impact, we embarked on a mission to bring clean water to Aponmu. The solution came in the form of a borehole—a source of hope that would transform the lives of the community forever.",
   "As the borehole was drilled and water began to flow, a wave of anticipation and joy swept through Aponmu. No longer bound by the arduous trek for water, families now had a reliable and sustainable source right at their doorstep. The burden on women and children lightened, replaced by the simple joy of turning a tap and witnessing the life-giving liquid flow freely.",
   "Beyond quenching their physical thirst, the borehole became a symbol of empowerment. With newfound time and energy, community members redirected their focus towards education and economic activities. Children spent more time in classrooms, and families engaged in initiatives to improve their economic well-being.",
@@ -9,6 +18,11 @@ export const aponmuBoreholeStory = [
 export const scholarshipStory = [
   "In the heart of Ife, Nigeria, where the sun casts its golden glow on dreams as vast as the African sky, a group of students from various schools in the vicinity faced the formidable challenge of financial constraints hindering their educational aspirations. Aspiring minds, rich with potential, found their dreams shrouded in uncertainty due to limited opportunities.",
   `Within this narrative of collective dreams encountering adversity, a beacon of hope emerged — the "Ignite Scholars" program. This scholarship initiative, rooted in faith and compassion, sought to illuminate the paths to success for the students grappling with financial barriers. It became a collective journey, where a group of students, bound by shared dreams, found solace and support through the teachings of Jesus.`,
+  {
+    type: "video",
+    src: educationVideo,
+    desc: "A documentry on the educational support provided by World Care International Ministry.",
+  },
   "Inspired by the essence of Jesus' teachings on love, compassion, and lifting the less privileged, the Ignite Scholars program transcended mere financial assistance. It became a community of support, where a collective of students discovered a family united by common dreams and a belief in the inherent potential within each student.",
   "The scholarship, a tangible manifestation of faith-infused initiatives, not only alleviated the financial burden for this group of students but also became a transformative force echoing the teachings of Jesus. The students' journey unfolded as rays of hope, collectively illuminating pathways to success and fostering an environment where faith played a pivotal role.",
   "Through the lens of Ignite Scholars, the collective dreams of students found realization. The scholarship not only provided tangible financial support for their education but also nurtured an atmosphere where faith became a guiding light. The program, embodying Jesus' emphasis on compassion and uplifting the less privileged, went beyond individual aspirations, becoming a collective story of shared success and communal upliftment.",
@@ -16,6 +30,11 @@ export const scholarshipStory = [
 export const osoloBoreholeStory = [
   "In the serene village of Osolo, nestled in the embrace of Akure, Nigeria, a profound transformation unfolded around a fundamental necessity — water. The community, blessed with the richness of Nigerian soil, faced a daily struggle that went beyond the challenges of agricultural endeavors. Fetching water became a laborious task, often from distant and contaminated streams, disproportionately affecting the women and children of Osolo.",
   `It is within this context of daily hardships that the "Living Waters Project" took root — a faith-inspired initiative aimed at bringing the blessing of clean water to the heart of Osolo. The teachings of Jesus on compassion and service echoed in the hearts of the project's founders, becoming the guiding light for a transformative endeavor`,
+  {
+    type: "image",
+    src: osoloOfOsolo,
+    desc: "The Osolo of Osolo with John and Angela at her abode.",
+  },
   "The borehole, a symbol of promise and life, emerged as a tangible manifestation of faith in action. As the Living Waters Project unfolded, the women of Osolo found liberation from the burdensome treks to distant water sources. No longer confined by the constraints of contaminated streams, the community flourished with newfound health, hope, and the precious gift of time.",
   "Inspired by the teachings of Jesus to quench the literal and metaphorical thirst of the less privileged, the Living Waters Project became a conduit for divine compassion. It wasn't merely a borehole; it was a source of life-changing promise, where the principles of faith became bricks in the foundation of a thriving community.",
   "The ripple effects of this initiative reached beyond the practicality of clean water. Children, once burdened by the task of fetching water, now attended school uninterrupted, embracing educational opportunities that became beacons of hope for Osolo's future. The borehole, a testament to the transformative power of faith-driven action, catalyzed a domino effect of positive change within the community.",
