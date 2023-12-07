309dba955f5adf6a47c4f8a47a641a7033290b49
Gboly <gbolahanahmed1@gmail.com>
2023-12-07 11:14:43 +0100

    fix: update render of story cameos in what-we-do

--- client/src/pages/what-we-do/WhatWeDo.jsx
+++ client/src/pages/what-we-do/WhatWeDo.jsx
@@ -4,7 +4,7 @@ import DonateButton from "../../components/donateButton/DonateButton";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Icon from "../../components/icon/Icon";
 import CustomSection from "../../components/customSection/customSection";
-import { useRef } from "react";
+import { useMemo, useRef } from "react";
 import { storyContent } from "../../util/content";
 import StoryCameo from "../../components/storyCameo/StoryCameo";
 import { Link } from "react-scroll";
@@ -14,6 +14,11 @@ const WhatWeDo = ({
   content: { id, title, poster, icon, snippet, content, video },
 }) => {
   const ourStoriesRef = useRef(null);
+
+  const thisDoingStories = useMemo(
+    () => storyContent.filter(({ category }) => category === id).slice(0, 3),
+    [id]
+  );
 
   return (
     <AnimatedPage className={`what-we-do ${id}`}>
@@ -40,7 +45,7 @@ const WhatWeDo = ({
       </section>
       {/* This section would require a fetch of stories relating to this service - the storyContent */}
       <CustomSection ref={ourStoriesRef} id={"our-stories"}>
-        {storyContent.map((content, index) => (
+        {thisDoingStories.map((content, index) => (
           <StoryCameo key={content.id} index={index} content={content} />
         ))}
       </CustomSection>
