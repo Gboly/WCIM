383a04e2db12b174be536bf90c71ff43b00abd84
Gboly <gbolahanahmed1@gmail.com>
2023-12-24 10:34:09 +0100

    feat: Create endpoints; getCountries, init paystack

--- client/src/app/api-slices/payment.js
+++ client/src/app/api-slices/payment.js
@@ -1,4 +1,5 @@
 import { apiSlice } from "../api";
+import axios from "axios";
 
 export const extendedPaymentApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
@@ -9,9 +10,30 @@ export const extendedPaymentApiSlice = apiSlice.injectEndpoints({
         body: credentials,
         credentials: "include",
       }),
+      transformResponse: (response) => response?.data,
       invalidatesTags: [{ type: "Paystack", id: "initialize" }],
+    }),
+
+    getCountriesAndStates: builder.query({
+      queryFn: async () => {
+        try {
+          const result = await axios.get(
+            "https://countriesnow.space/api/v0.1/countries/states"
+          );
+          return { data: result.data.data };
+        } catch (axiosError) {
+          let err = axiosError;
+          return {
+            error: {
+              status: err.response?.status,
+              data: err.response?.data || err.message,
+            },
+          };
+        }
+      },
     }),
   }),
 });
 
-export const { useInitPaystackMutation } = extendedPaymentApiSlice;
+export const { useInitPaystackMutation, useGetCountriesAndStatesQuery } =
+  extendedPaymentApiSlice;
