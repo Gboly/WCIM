e6051c1027cf85024e9de390561d44eeabb15c26
Gboly <gbolahanahmed1@gmail.com>
2023-12-25 21:07:37 +0100

    feat: Introduce the subscription feature

--- server/controllers/paystackPayment.js
+++ server/controllers/paystackPayment.js
@@ -2,31 +2,72 @@ import axios from "axios";
 import dotenv from "dotenv";
 import Donation from "../models/donation.js";
 dotenv.config();
+const authorization = `Bearer ${process.env.SECRET_KEY}`;
 
 const initOptions = {
   method: "POST",
   url: "https://api.paystack.co/transaction/initialize",
   port: 443,
   headers: {
-    authorization: `Bearer ${process.env.SECRET_KEY}`,
+    authorization: authorization,
     "Content-Type": "application/json",
   },
 };
 
 const verificationOptions = {
   method: "GET",
-  url: "https://api.paystack.co/transaction/verify/6uyxyg6bws",
+  url: "https://api.paystack.co/transaction/verify/",
   port: 443,
   headers: {
-    authorization: `Bearer ${process.env.SECRET_KEY}`,
+    authorization: authorization,
+  },
+};
+
+const listPlansOptions = {
+  url: "https://api.paystack.co/plan",
+  port: 443,
+  method: "GET",
+  headers: {
+    Authorization: authorization,
+  },
+};
+
+const createPlanOptions = {
+  url: "https://api.paystack.co/plan",
+  port: 443,
+  method: "POST",
+  headers: {
+    Authorization: authorization,
+    "Content-Type": "application/json",
   },
 };
 
 export const initializePayment = async (req, res) => {
-  const { email, amount, currency } = req.body;
+  const { email, amount, currency, subscription } = req.body;
   try {
     const donation = new Donation(req.body);
     await donation.save();
+
+    let planCode;
+    if (subscription) {
+      const plans = await axios(listPlansOptions);
+      const desiredPlan = plans.data.data.find(
+        ({ amount: price }) => String(price) === amount
+      );
+      if (!desiredPlan) {
+        const plan = await axios({
+          ...createPlanOptions,
+          data: JSON.stringify({
+            name: `${Number(amount) / 100} donations`,
+            interval: "monthly",
+            amount,
+          }),
+        });
+        plan && (planCode = plan.data.data.plan_code);
+      } else {
+        planCode = desiredPlan.plan_code;
+      }
+    }
 
     const initResponse = await axios({
       ...initOptions,
@@ -34,6 +75,7 @@ export const initializePayment = async (req, res) => {
         email,
         amount,
         currency,
+        ...(planCode ? { plan: planCode } : {}),
       }),
     });
     res.status(200).json(initResponse.data);
@@ -46,8 +88,13 @@ export const initializePayment = async (req, res) => {
 };
 
 export const verifyPayment = async (req, res) => {
+  const { reference } = req.body;
+
   try {
-    const verificationResponse = await axios(verificationOptions);
+    const verificationResponse = await axios({
+      ...verificationOptions,
+      url: `${verificationOptions.url}${reference}`,
+    });
     res.status(200).json(verificationResponse.data);
   } catch (error) {
     console.log(error);
