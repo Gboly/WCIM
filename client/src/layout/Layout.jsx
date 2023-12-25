79d33b45d4dc76f3fb8042ae996cc4cbdc71ce97
Gboly <gbolahanahmed1@gmail.com>
2023-12-25 21:07:15 +0100

    feat: Incorporate Donate section to all pages

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
+import Donate from "../components/donate/Donate";
 
 export default function Layout() {
   return (
     <>
       <Navbar />
       <Outlet />
+      <Donate />
       <Footer />
     </>
   );
