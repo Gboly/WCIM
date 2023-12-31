bf2da3d675d10d1f2e646bbb7aa16e369f61f6ed
Gboly <gbolahanahmed1@gmail.com>
2023-12-31 22:05:51 +0100

    fix: Use fetched stories to render story page

--- client/src/pages/stories/Stories.jsx
+++ client/src/pages/stories/Stories.jsx
@@ -1,20 +1,26 @@
 import "./stories.css";
-import { useMemo, useRef } from "react";
+import { useRef } from "react";
 import CustomSection from "../../components/customSection/customSection";
 import { useParams } from "react-router-dom";
-import { storyContent } from "../../util/content";
 import AnimatedPage from "../../components/animated/AnimatedPage";
 import StoryCameo from "../../components/storyCameo/StoryCameo";
+import {
+  useGetStoriesByQueryQuery,
+  useGetStoryByNameQuery,
+} from "../../app/api-slices/story";
+import Spinner from "../../components/spinner/Spinner";
 
 const Stories = () => {
   const storyRef = useRef(null);
   const moreStoriesRef = useRef(null);
   const { name } = useParams();
 
-  const stories = useMemo(
-    () => storyContent.find(({ name: storyName }) => storyName === name),
-    [name]
-  );
+  const { data: story } = useGetStoryByNameQuery({ start: 0, end: 1, name });
+  const { data: moreStories } = useGetStoriesByQueryQuery({
+    start: 0,
+    end: 3,
+    query: `name_ne=${name}`,
+  });
 
   const storyMedia = ({ type, src, desc }, index) => (
     <div className="media-container" key={index}>
@@ -28,32 +34,36 @@ const Stories = () => {
   );
   return (
     <AnimatedPage className={"story-page"}>
-      <CustomSection id={stories?.desc || ""} ref={storyRef}>
-        <img src={stories?.imgSrc || ""} alt="" className="story-poster" />
-        <p className="date">{new Date().toLocaleString()}</p>
-        <div>
-          {(stories?.body || []).map((content, index) =>
-            content?.type ? (
-              storyMedia(content, index)
+      {story ? (
+        <>
+          <CustomSection id={story.title || ""} ref={storyRef}>
+            <img src={story.poster || ""} alt="" className="story-poster" />
+            {/* <p className="date">{new Date(story.createAt).toLocaleString()}</p> */}
+            <div>
+              {(story.story || []).map((content, index) =>
+                content?.type ? (
+                  storyMedia(content, index)
+                ) : (
+                  <p key={index} className="content">
+                    {content}
+                  </p>
+                )
+              )}
+            </div>
+          </CustomSection>
+          <CustomSection id="more-stories" ref={moreStoriesRef}>
+            {moreStories ? (
+              moreStories.map((content, index) => (
+                <StoryCameo key={content._id} index={index} content={content} />
+              ))
             ) : (
-              <p key={index} className="content">
-                {content}
-              </p>
-            )
-          )}
-        </div>
-      </CustomSection>
-      <CustomSection id="more-stories" ref={moreStoriesRef}>
-        {storyContent
-          .filter(
-            ({ name: storyName }, index) => storyName !== name && index < 3
-          )
-          .map((content, index) => (
-            <StoryCameo key={content.id} index={index} content={content} />
-          ))}
-      </CustomSection>
-
-      {/* Donate section */}
+              <Spinner />
+            )}
+          </CustomSection>
+        </>
+      ) : (
+        <Spinner />
+      )}
     </AnimatedPage>
   );
 };
