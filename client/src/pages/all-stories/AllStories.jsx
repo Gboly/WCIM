583cddb6b665df1d798c74436c35172f7be2d5e9
Gboly <gbolahanahmed1@gmail.com>
2023-12-31 19:47:20 +0100

    fix: Fetch stories for allStories page

--- client/src/pages/all-stories/AllStories.jsx
+++ client/src/pages/all-stories/AllStories.jsx
@@ -1,20 +1,37 @@
-import { useContext, useMemo, useRef } from "react";
+import { useContext, useRef } from "react";
 import AnimatedPage from "../../components/animated/AnimatedPage";
 import CustomSection from "../../components/customSection/customSection";
 import "./all-stories.css";
 import { storyCategories } from "../../util/content";
-import { getStoryCategory } from "../../util/functions";
 import StoryCameo from "../../components/storyCameo/StoryCameo";
 import { GeneralContext } from "../../App";
+import {
+  useGetStoriesByCategoryQuery,
+  useGetStoriesQuery,
+} from "../../app/api-slices/story";
+import Spinner from "../../components/spinner/Spinner";
 
+const range = { start: 0, end: 10 };
 const AllStories = () => {
   const { storyCategory, changeStoryCategory } = useContext(GeneralContext);
   const allStoriesRef = useRef(null);
 
-  const stories = useMemo(
-    () => getStoryCategory(storyCategory),
-    [storyCategory]
+  // Implement pagination for this
+  const { data: allStories, isLoading: storyIsLoading1 } = useGetStoriesQuery(
+    range,
+    {
+      skip: storyCategory !== "show all",
+    }
   );
+  const { data: categoryStories, isLoading: storyIsLoading } =
+    useGetStoriesByCategoryQuery(
+      { ...range, category: storyCategory },
+      {
+        skip: storyCategory === "show all",
+      }
+    );
+
+  const stories = storyCategory === "show all" ? allStories : categoryStories;
 
   return (
     <AnimatedPage className={"all-stories"}>
@@ -31,9 +48,11 @@ const AllStories = () => {
             </button>
           ))}
         </div>
-        {stories.map((content, index) => (
-          <StoryCameo key={content.id} index={index} content={content} />
-        ))}
+        {stories &&
+          stories.map((content, index) => (
+            <StoryCameo key={content._id} index={index} content={content} />
+          ))}
+        {(storyIsLoading || storyIsLoading1) && <Spinner />}
       </CustomSection>
     </AnimatedPage>
   );
