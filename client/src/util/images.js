69c95961670eec0fa526204f7a95159b82afb300
Gboly <gbolahanahmed1@gmail.com>
2024-01-01 02:52:42 +0100

    fix: Provide respective cloudinary image links

---
+++ client/src/util/images.js
@@ -0,0 +1,48 @@
+export const boreholeAponmu =
+  "https://res.cloudinary.com/dirwr8cde/image/upload/v1703697187/wcim/borehole-aponmu_lqhjer.png";
+export const evangelismStoryPoster =
+  "https://res.cloudinary.com/dirwr8cde/image/upload/v1703697315/wcim/wcim-evangelism4_cbccbs.jpg";
+export const schoolAmenitiesPoster =
+  "https://res.cloudinary.com/dirwr8cde/image/upload/v1703697314/wcim/wcim-school-amenities_dbls7j.jpg";
+export const osoloOfOsolo =
+  "https://res.cloudinary.com/dirwr8cde/image/upload/v1703697261/wcim/wcim-osolo_prmxzk.jpg";
+export const orphanGiftPoster =
+  "https://res.cloudinary.com/dirwr8cde/image/upload/v1703697259/wcim/wcim-orphan2_tco9w7.jpg";
+export const scholarshipPoster =
+  "https://res.cloudinary.com/dirwr8cde/image/upload/v1703697276/wcim/wcim-scholarship2_sacq1d.jpg";
+export const feedPoster =
+  "https://res.cloudinary.com/dirwr8cde/image/upload/v1703697279/wcim/wcim-donations_mlkrlk.jpg";
+export const monthlySupport =
+  "https://res.cloudinary.com/dirwr8cde/image/upload/v1703697280/wcim/wcim-monthly_tgnzb8.jpg";
+export const waterGiftPoster =
+  "https://res.cloudinary.com/dirwr8cde/image/upload/v1703697251/wcim/wcim-water-gift_eltncg.jpg";
+export const healPoster =
+  "https://res.cloudinary.com/dirwr8cde/image/upload/v1703697271/wcim/wcim-healPoster_v4lvhq.jpg";
+export const scholarship =
+  "https://res.cloudinary.com/dirwr8cde/image/upload/v1703697309/wcim/wcim-scholarship_kcovki.jpg";
+export const healthPoster =
+  "https://res.cloudinary.com/dirwr8cde/image/upload/v1703697296/wcim/wcim-health4_u1lawq.jpg";
+export const orphanPoster =
+  "https://res.cloudinary.com/dirwr8cde/image/upload/v1703697307/wcim/wcim-orphan_ghuyvs.jpg";
+export const financingPoster =
+  "https://res.cloudinary.com/dirwr8cde/image/upload/v1703697257/wcim/wcim-financing_rqgpj4.jpg";
+export const cleanWaterVideo =
+  "https://res.cloudinary.com/dirwr8cde/video/upload/v1703697396/wcim/clean-water-vid_jfn3nk.mp4";
+export const educationVideo =
+  "https://res.cloudinary.com/dirwr8cde/video/upload/v1703697395/wcim/education-vid_t9keqr.mp4";
+export const evangelismPoster =
+  "https://res.cloudinary.com/dirwr8cde/image/upload/v1703697304/wcim/wcim-evangelism3_g4c3fl.jpg";
+export const boreholeOsolo =
+  "https://res.cloudinary.com/dirwr8cde/image/upload/v1703697284/wcim/wcim-water_lt8d1v.jpg";
+export const eduPoster =
+  "https://res.cloudinary.com/dirwr8cde/image/upload/v1703697312/wcim/wcim-edu2_wccjgn.jpg";
+export const official1 =
+  "https://res.cloudinary.com/dirwr8cde/image/upload/v1703697305/wcim/wcim-official1_hwqxto.jpg";
+export const official2 =
+  "https://res.cloudinary.com/dirwr8cde/image/upload/v1703697281/wcim/wcim-official2_pgvqb8.jpg";
+export const official3 =
+  "https://res.cloudinary.com/dirwr8cde/image/upload/v1703697301/wcim/wcim-official3_u91ejf.jpg";
+export const wcimMovement =
+  "https://res.cloudinary.com/dirwr8cde/image/upload/v1703697268/wcim/wcim-action_r5x8sg.jpg";
+export const givingPoster =
+  "https://res.cloudinary.com/dirwr8cde/image/upload/v1703697258/wcim/wcim-giving-poster_ml5s70.png";
