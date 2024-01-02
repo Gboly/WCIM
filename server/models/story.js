e26e87b784b66af9531c005793805aa6afe15023
Gboly <gbolahanahmed1@gmail.com>
2024-01-02 18:01:07 +0100

    feat: Make desired schema field have text index

--- server/models/story.js
+++ server/models/story.js
@@ -4,15 +4,22 @@ const schema = mongoose.Schema;
 
 const storySchema = schema({
   poster: { type: String, required: true },
-  name: { type: String, required: true },
-  title: { type: String, required: true },
-  snippet: { type: String, required: true },
-  story: { type: [schema.Types.Mixed], required: true },
-  category: { type: String, required: true },
+  name: { type: String, required: true, index: "text" },
+  title: { type: String, required: true, index: "text" },
+  snippet: { type: String, required: true, index: "text" },
+  story: { type: [schema.Types.Mixed], required: true, index: "text" },
+  category: { type: String, required: true, index: "text" },
   url: { type: String, required: true },
-  keywords: { type: [{ type: String }], required: true },
+  keywords: { type: [{ type: String }], required: true, index: "text" },
   createdAt: { type: Date, default: new Date() },
 });
+
+// storySchema.index(
+//   ["name", "title", "story", "category", "keywords"].reduce((accum, key) => {
+//     accum = { ...accum, [key]: "text" };
+//     return accum;
+//   }, {})
+// );
 
 const Story = mongoose.model("story", storySchema);
 
