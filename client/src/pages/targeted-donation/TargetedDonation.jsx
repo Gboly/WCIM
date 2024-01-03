0efd2a6f21f80c997562ebaac307300f336888d8
Gboly <gbolahanahmed1@gmail.com>
2024-01-03 23:11:25 +0100

    feat: Create the targted donation component

---
+++ client/src/pages/targeted-donation/TargetedDonation.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useMemo, useRef } from "react";
+import AnimatedPage from "../../components/animated/AnimatedPage";
+import CustomSection from "../../components/customSection/customSection";
+import "./targeted-donation.css";
+import { useOutletContext, useParams } from "react-router-dom";
+import Donate from "../../components/donate/Donate";
+import { giftCategories } from "../../util/content";
+import line from "../../assets/thread-line.png";
+
+const TargetedDonation = () => {
+  const donateRef = useRef(null);
+  const { id } = useParams();
+  // Pardon the naming. Should have made it dynamic.
+  const { setIsNoPage } = useOutletContext();
+
+  useEffect(() => {
+    setIsNoPage(true);
+
+    return () => setIsNoPage(false);
+  }, [setIsNoPage]);
+
+  const { poster, title, desc } = useMemo(() => {
+    return giftCategories.find(({ id: giftId }) => giftId === id);
+  }, [id]);
+
+  return (
+    <AnimatedPage className={"targeted-donation"}>
+      <CustomSection id={""} ref={donateRef}>
+        <div className="intro">
+          <h3>{title}</h3>
+          <img src={line} alt="" />
+          <p>{desc}</p>
+        </div>
+      </CustomSection>
+      <Donate bgImage={poster} giftCategory={title} />
+    </AnimatedPage>
+  );
+};
+
+export default TargetedDonation;
