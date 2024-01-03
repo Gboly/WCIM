86eff1bff6431b07edad4d5b78bd029aa1ccf642
Gboly <gbolahanahmed1@gmail.com>
2024-01-03 09:42:11 +0100

    feat: Create the error boundary component

---
+++ client/src/components/error/Error.jsx
@@ -0,0 +1,22 @@
+import "./error.css";
+import errorImage from "../../assets/error_image.png";
+import AnimatedPage from "../animated/AnimatedPage";
+import CustomSection from "../customSection/customSection";
+import { useRef } from "react";
+
+const Error = () => {
+  const errorRef = useRef(null);
+
+  return (
+    <AnimatedPage className={"error-page"}>
+      <CustomSection id={""} ref={errorRef}>
+        <img src={errorImage} alt="" />
+        <h3>Something went wrong...</h3>
+        <p>We are working on fixing the problem.</p>
+        <button onClick={() => location.reload()}>Try again</button>
+      </CustomSection>
+    </AnimatedPage>
+  );
+};
+
+export default Error;
