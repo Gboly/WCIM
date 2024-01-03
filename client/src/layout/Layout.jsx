79866c85166818837beec9499b84028da1bcbe9e
Gboly <gbolahanahmed1@gmail.com>
2024-01-03 09:42:43 +0100

    feat: Incorporate the error boundary at Layout level

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -1,14 +1,29 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
 import Donate from "../components/donate/Donate";
+import { ErrorBoundary } from "react-error-boundary";
+import Error from "../components/error/Error";
+
+const donateExcludedPages = [
+  "who-we-are/team",
+  "/donate/checkout",
+  "/search",
+  "who-we-are/contact-us",
+];
 
 export default function Layout() {
+  const location = useLocation();
+
+  const isExcluded = donateExcludedPages.includes(location.pathname);
+
   return (
     <>
       <Navbar />
-      <Outlet />
-      <Donate />
+      <ErrorBoundary FallbackComponent={Error}>
+        <Outlet />
+        {!isExcluded && <Donate />}
+      </ErrorBoundary>
       <Footer />
     </>
   );
