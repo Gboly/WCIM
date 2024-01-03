82ac4eda4ce664d779b34e0287118fff1734dc7f
Gboly <gbolahanahmed1@gmail.com>
2024-01-03 18:31:49 +0100

    fix: Exclude donate page from noPage route

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -4,24 +4,27 @@ import Footer from "../components/footer/Footer";
 import Donate from "../components/donate/Donate";
 import { ErrorBoundary } from "react-error-boundary";
 import Error from "../components/error/Error";
+import { useState } from "react";
 
 const donateExcludedPages = [
-  "who-we-are/team",
+  "/who-we-are/team",
   "/donate/checkout",
   "/search",
-  "who-we-are/contact-us",
+  "/who-we-are/contact-us",
 ];
 
 export default function Layout() {
   const location = useLocation();
+  const [isNoPage, setIsNoPage] = useState(false);
 
-  const isExcluded = donateExcludedPages.includes(location.pathname);
+  const isExcluded =
+    donateExcludedPages.includes(location.pathname) || isNoPage;
 
   return (
     <>
       <Navbar />
       <ErrorBoundary FallbackComponent={Error}>
-        <Outlet />
+        <Outlet context={{ setIsNoPage, isNoPage }} />
         {!isExcluded && <Donate />}
       </ErrorBoundary>
       <Footer />
