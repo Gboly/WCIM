013261cd37dcf58912e267cf114e17e37668b38f
Gboly <gbolahanahmed1@gmail.com>
2024-01-06 11:53:16 +0100

    feat: Use prod urls respectively 4 client & server

--- client/src/app/api.js
+++ client/src/app/api.js
@@ -4,7 +4,9 @@ import { isDev } from "../util/functions";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: isDev ? "http://localhost:5000" : "https://wcim.vercel.app",
+    baseUrl: isDev
+      ? "http://localhost:5000"
+      : "https://wcim-backend.vercel.app",
   }),
   tagTypes: ["Paystack", "Story"],
   endpoints: () => ({}),
