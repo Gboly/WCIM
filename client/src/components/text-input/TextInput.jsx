8da227ba158f86f04ad43dfba3f3f45004c52411
Gboly <gbolahanahmed1@gmail.com>
2024-01-15 09:46:23 +0100

    fix: Introduce more props to TextIput

--- client/src/components/text-input/TextInput.jsx
+++ client/src/components/text-input/TextInput.jsx
@@ -2,24 +2,33 @@ import { capitalizeCamelCase } from "../../util/functions";
 import "./text-input.css";
 
 const inputTypes = {
-  firstName: "text",
-  lastName: "text",
   email: "email",
-  address: "text",
+  guardianEmail: "email",
+  DOB: "date",
+  lastResult: "file",
+  essay: "file",
 };
-const TextInput = ({ value, handleInput, name }) => {
+const accept = {
+  essay:
+    "application/pdf,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+  lastResult: "image/png, image/gif, image/jpeg",
+};
+const TextInput = ({ value, handleInput, handleChange, name, label }) => {
   return (
     <div className="text-input-container">
       <label htmlFor={name}>
-        {capitalizeCamelCase(name) +
-          (name === "address" ? " (optional)" : " *")}
+        {label ||
+          capitalizeCamelCase(name) +
+            (name === "address" ? " (optional)" : " *")}
       </label>
       <input
-        type={inputTypes[name]}
+        type={inputTypes[name] || "text"}
         id={name}
         value={value}
         onInput={handleInput}
+        onChange={handleChange}
         autoComplete="off"
+        accept={accept[name] || "*"}
       />
     </div>
   );
